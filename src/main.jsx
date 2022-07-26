a9f24e22b2a1ba3b4e0aa9e252db94d5c06f3ab4
Ainsdev <ainsn1654@gmail.com>
2022-07-26 13:08:08 -0400

    adding home to routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AppBusinessCard from './App_businessCard'
+import AppBusinessCard from './components_businessCard/App_businessCard'
 import './index.css'
+import Home from './Home.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
