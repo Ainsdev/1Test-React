ebca00bfa88d83e909f9fa7e224a81007b3b553c
Ainsdev <ainsn1654@gmail.com>
2022-08-12 19:06:04 -0400

    Business Logic/Delay problem

--- src/components_notes/AppNote.jsx
+++ src/components_notes/AppNote.jsx
@@ -1,10 +1,9 @@
 import AddNote from "./AddNote";
 import Notes from "./Notes";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function AppNotes() {
-    var storage = window.localStorage;
-    const [notes, setNotes] = useState([{title: "Thanks for watching", text: "created by: Guilad", date: "2022/03/08"}]);
+    const [notes, setNotes] = useState([{ title: "Project Created by: Guilad", text: "", date: "2022/03/08" }]);
     return (
         <main className="bg-blue-300">
             <header className="w-screen p-5 font-semibold text-xl">
--- src/components_notes/Notes.jsx
+++ src/components_notes/Notes.jsx
@@ -1,12 +1,20 @@
 import Note from "./Note";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 export default function Notes(params) {
-    let data = params.data.map(item => <Note title={item.title} text={item.text} date={item.date}/>);
+    const storage = window.localStorage;
+    var notes = JSON.parse(storage.getItem("notes"));
+    let data = notes.map(item => <Note title={item.title} text={item.text} date={item.date} />);
+    //managing data
+    // storage.getItem("notes") ? JSON.parse(storage.getItem("notes")) :
+    useEffect(() => {
+        storage.setItem("notes", JSON.stringify(params.data));
+
+    }, [notes])
     return (
-        <article className="m-1 mt-5 text-black pt-5 sm:w-1/3">
+        <article className="m-1 mt-5 text-black pt-5 sm:w-1/3 flex flex-col">
             <h1 className="text-2xl font-bold">Notes</h1>
-            {data.length > 1 ? data : <p className="text-center">Write your first note!!</p>}
+            {data.length > 1 ? data : <p className="text-center animate-bounce text-xl">Write your first note!!</p>}
         </article>
     )
 }
